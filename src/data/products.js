3d70d118873ba1a3823fd720a7038d7ef8fc9f83
jaimru-farzan <farzan@jaimru.com>
2024-03-09 12:11:54 +0530

    sample product data added

---
+++ src/data/products.js
@@ -0,0 +1,62 @@
+export const products = [
+  {
+    id: 1,
+    name: "iQOO Z7 Pro 5G (Blue Lagoon, 8GB RAM, 256GB Storage) ",
+    price: "1000",
+    img: "/products/product-1.jpg",
+  },
+  {
+    id: 2,
+    name: "Campus Mens Crysta ProRunning Shoes",
+    price: "500",
+    img: "/products/product-2.jpg",
+  },
+  {
+    id: 3,
+    name: "Pacific Handicrafts Wooden Intersecting Floating Shelves",
+    price: "1200",
+    img: "/products/product-3.jpg",
+  },
+  {
+    id: 4,
+    name: "MI 80 cm (32 inches) A Series HD Ready Smart Google TV",
+    price: "12000",
+    img: "/products/product-4.jpg",
+  },
+  {
+    id: 5,
+    name: "The Ultimate Household Tool Set",
+    price: "1200",
+    img: "/products/product-5.jpg",
+  },
+  {
+    id: 6,
+    name: "Satpurush Bathroom Accessories",
+    price: "500",
+    img: "/products/product-6.jpg",
+  },
+  {
+    id: 7,
+    name: "UNIQUELLA Classic Crystal Curtain Hold Backs",
+    price: "600",
+    img: "/products/product-7.jpg",
+  },
+  {
+    id: 8,
+    name: "Al Rehab Choco Musk Attar of 6ml",
+    price: "350",
+    img: "/products/product-8.jpg",
+  },
+  {
+    id: 9,
+    name: "Unique Book Care Centre Valentine Romantic Love Couple Statue",
+    price: "4500",
+    img: "/products/product-9.jpg",
+  },
+  {
+    id: 10,
+    name: "L Shape Sofa Set for Living Room",
+    price: "9000",
+    img: "/products/product-10.jpg",
+  },
+];
